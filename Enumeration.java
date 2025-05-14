966cee3f85261abd675dffbee00bfa71aaaeec86
DamonNing <853003574@qq.com>
2025-05-14 16:07:36 +0800

    Enumation

---
+++ Enumeration.java
@@ -0,0 +1,44 @@
+public class Enumeration {
+    public static void main(String[] args) {
+        System.out.println(Season.SPRING);
+        System.out.println(Season.SUMMER);
+        System.out.println(Season.AUTUMN);
+        System.out.println(Season.WINTER);
+    }
+}
+enum Season {
+    SPRING("春天","warm"),SUMMER("夏天","hot"),
+    AUTUMN("秋天","cool"),WINTER("冬天","cold");
+
+    private String name;
+    private String desc;
+
+    private Season(String name, String desc) {
+        this.name = name;
+        this.desc = desc;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    @Override
+    public String toString() {
+        return "Season{" +
+                "name='" + name + '\'' +
+                ", desc='" + desc + '\'' +
+                '}';
+    }
+}
